df683941d619913b83a00ed2272bebae9caebe02
ChaseWelsh <fseusb@gmail.com>
2022-08-29 10:33:34 -0600

    commit tree 8f15c6cd008a89781b31a58304e22bf1aac39e83

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>What the did you just say about me you little ? I'll have you know I graduated top of my class in the navy seals and have over 300 confirmed kills in gorilla warfare. You are nothing to me but another target. You're ing dead kiddo.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
